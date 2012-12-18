35a5c4c4337a483283204de0446a2fff6d959862
Rich Turner <rich@bitcrazed.com>
2012-12-18 11:25:52 -0800

    Removed commented out code

--- vagrant/tools/chocolateyInstall.ps1
+++ vagrant/tools/chocolateyInstall.ps1
@@ -1,10 +1,2 @@
-#try {
-	#Install vagrant and related windows dependencies
-#	jgem install jruby-openssl jruby-win32ole vagrant
-    Install-ChocolateyPackage 'Vagrant' 'MSI' '-q' 'http://files.vagrantup.com/packages/be0bc66efc0c5919e92d8b79e973d9911f2a511f/vagrant_1.0.5.msi'
-	
-#    Write-ChocolateySuccess 'Vagrant'
-#} catch {
-#    Write-ChocolateyFailure 'Vagrant' $($_.Exception.Message)
-#    throw 
-#}
+Install-ChocolateyPackage 'Vagrant' 'MSI' '-q' 'http://files.vagrantup.com/packages/be0bc66efc0c5919e92d8b79e973d9911f2a511f/vagrant_1.0.5.msi'
+
